30d62c2e27840738de77857a0cdda9ab67caa784
vihermawan <vickyhermawan99@mail.ugm.ac.id>
2021-02-03 11:50:29 +0700

    add api employee

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,8 @@
-import '../styles/globals.css'
+import { wrapper } from "../store"
 import "../styles/main.scss";
+
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
 }
 
-export default MyApp
+export default wrapper.withRedux(MyApp);
