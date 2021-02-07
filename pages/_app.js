abd49998dcd2006ec063550afd2ad708f5064d99
vihermawan <vickyhermawan99@mail.ugm.ac.id>
2021-02-07 21:05:56 +0700

    add tailwind

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import { wrapper } from "../store"
 import "../styles/main.scss";
+import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
